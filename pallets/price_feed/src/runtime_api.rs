b41c466d6282c27b977cbaf0ccaf9e3330eb7611
olegnn <olegnosov1@gmail.com>
2023-01-20 12:13:31 +0100

    Remove unneeded type parameter

--- pallets/price_feed/src/runtime_api.rs
+++ pallets/price_feed/src/runtime_api.rs
@@ -1,10 +1,9 @@
 use codec::{Decode, Encode};
-use frame_support::traits::Get;
 use price_provider::{CurrencySymbolPair, PriceRecord};
 use scale_info::prelude::string::String;
 
 sp_api::decl_runtime_apis! {
-    pub trait PriceFeedApi<T: Encode + Decode, MaxLen: Get<u32>> {
+    pub trait PriceFeedApi<T: Encode + Decode> {
         /// Gets the price of the given pair from pallet's storage
         fn price(pair: CurrencySymbolPair<String, String>) -> Option<PriceRecord<T>>;
     }
